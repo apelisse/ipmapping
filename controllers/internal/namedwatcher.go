ab0d9b76b84c34da08a98deaa62de3ecaecc03d2
Antoine Pelisse <apelisse@gmail.com>
2022-05-24 15:34:58 -0700

    Add NamedWatcher to watch GVRNNs

---
+++ controllers/internal/namedwatcher.go
@@ -0,0 +1,177 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+)
+
+// NamedWatcher watches a specific gvrnn and calls the handler with no
+// parameters (since the key should already be known by the handler).
+//
+// This internally maintains a list of watches for each GVRN and
+// dispatch by name accordingly. When a watch is stopped, it doesn't
+// necessarily stops the underlying watch if another name in the same
+// gvrn is still being watched. The watch is removed once the GVRN
+// doesn't need to be watched anymore.
+type NamedWatcher interface {
+	// Watch creates and starts a new watch for the given GVRNN. If
+	// the watch can't be started, an error is returned. The watch
+	// can be stopped by calling its Stop method.
+	//
+	// The NamedWatcher can't be used from within the handler
+	// (neither to start a new watch nor to stop a watch) in order
+	// to avoid deadlocks.
+	Watch(gvr schema.GroupVersionResource, namespace string, name string, handler func() error) (Watch, error)
+}
+
+func NewNamedWatcher(watchManager WatchManager, log logr.Logger) NamedWatcher {
+	return &namedWatcher{
+		watchManager: watchManager,
+		watches:      make(map[gvrn]*gvrnWatch),
+		log:          log.WithName("NamedWatcher"),
+	}
+}
+
+type gvrn struct {
+	gvr       schema.GroupVersionResource
+	namespace string
+}
+
+type namedWatcher struct {
+	watchManager WatchManager
+	watches      map[gvrn]*gvrnWatch
+	log          logr.Logger
+	mutex        sync.Mutex
+}
+
+func (n *namedWatcher) newGVRNWatch(gvr schema.GroupVersionResource, namespace string) (*gvrnWatch, error) {
+	gvrnWatch := gvrnWatch{
+		names: map[string][]*namedWatch{},
+	}
+	var err error
+	gvrnWatch.watch, err = n.watchManager.Watch(gvr, namespace, gvrnWatch.handle)
+	if err != nil {
+		return nil, err
+	}
+	return &gvrnWatch, nil
+}
+
+func (n *namedWatcher) Watch(gvr schema.GroupVersionResource, namespace string, name string, handler func() error) (Watch, error) {
+	n.log.Info("Adding new watch", "gvr", gvr, "namespace", namespace, "name", name)
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	gvrn := gvrn{gvr: gvr, namespace: namespace}
+	watch, ok := n.watches[gvrn]
+	if !ok {
+		var err error
+		watch, err = n.newGVRNWatch(gvr, namespace)
+		if err != nil {
+			return nil, err
+		}
+		n.watches[gvrn] = watch
+	}
+	namedWatch := &namedWatch{
+		watcher:   n,
+		handler:   handler,
+		gvr:       gvr,
+		namespace: namespace,
+		name:      name,
+	}
+	watch.add(name, namedWatch)
+	return namedWatch, nil
+}
+
+func (n *namedWatcher) remove(watch *namedWatch) {
+	n.log.Info("Removing watch", "gvr", watch.gvr, "namespace", watch.namespace, "name", watch.name)
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	gvrn := gvrn{gvr: watch.gvr, namespace: watch.namespace}
+	gvrnWatch, ok := n.watches[gvrn]
+	if !ok {
+		panic(fmt.Errorf("Trying to remove unknown watch: %v", gvrn))
+	}
+	if gvrnWatch.remove(watch.name, watch) {
+		delete(n.watches, gvrn)
+	}
+}
+
+type gvrnWatch struct {
+	watch Watch
+	// For each name, we can have multiple watches.
+	names map[string][]*namedWatch
+}
+
+// Remove the given name from the watch list.
+// Returns true if this was the last item present and the watch
+// has been cleared.
+func (g *gvrnWatch) remove(name string, watch *namedWatch) bool {
+	if watches, ok := g.names[name]; ok {
+		for i := range watches {
+			if watches[i] == watch {
+				g.names[name] = append(watches[:i], watches[i+1:]...)
+				break
+			}
+		}
+		if len(g.names[name]) == 0 {
+			delete(g.names, name)
+		}
+		if len(g.names) == 0 {
+			g.watch.Stop()
+			return true
+		}
+	}
+	return false
+}
+
+func (g *gvrnWatch) handle(name string) error {
+	hasErr := false
+	for _, watches := range g.names[name] {
+		err := watches.handler()
+		if err != nil {
+			utilruntime.HandleError(err)
+			hasErr = true
+		}
+	}
+	if hasErr {
+		return fmt.Errorf("Error processing item: %v", name)
+	}
+	return nil
+}
+
+// Append a new watch for that name.
+func (g *gvrnWatch) add(name string, watch *namedWatch) {
+	g.names[name] = append(g.names[name], watch)
+}
+
+type namedWatch struct {
+	watcher   *namedWatcher
+	handler   func() error
+	gvr       schema.GroupVersionResource
+	namespace string
+	name      string
+}
+
+// Stop the given namedWatch.
+func (n *namedWatch) Stop() {
+	n.watcher.remove(n)
+}
---
+++ controllers/internal/namedwatcher_test.go
@@ -0,0 +1,261 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers_test
+
+import (
+	"fmt"
+	"testing"
+
+	controllers "change.me.later/ipmapping/controllers/internal"
+	"github.com/go-logr/logr"
+	"github.com/go-logr/logr/testr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type FakeWatchManager struct {
+	watches map[schema.GroupVersionResource]map[string]FakeWatch
+	log     logr.Logger
+}
+
+func NewFakeWatchManager(log logr.Logger) *FakeWatchManager {
+	return &FakeWatchManager{
+		watches: make(map[schema.GroupVersionResource]map[string]FakeWatch),
+		log:     log.WithName("FakeWatchManager"),
+	}
+}
+
+func (f *FakeWatchManager) Watch(gvr schema.GroupVersionResource, namespace string, handler func(key string) error) (controllers.Watch, error) {
+	f.log.Info("Watch", "gvr", gvr, "namespace", namespace)
+	if gvrw, ok := f.watches[gvr]; ok {
+		if _, ok := gvrw[namespace]; ok {
+			panic(fmt.Errorf("Watch is already set-up for %v/%v", gvr, namespace))
+		}
+	} else {
+		f.watches[gvr] = make(map[string]FakeWatch)
+	}
+	watch := FakeWatch{
+		manager:   f,
+		gvr:       gvr,
+		namespace: namespace,
+		handler:   handler,
+		log:       f.log.WithName("FakeWatch"),
+	}
+	f.watches[gvr][namespace] = watch
+	return &watch, nil
+}
+
+// Calls an event on the given name for the watch.
+func (f *FakeWatchManager) Event(gvr schema.GroupVersionResource, namespace string, name string) {
+	f.log.Info("Event", "gvr", gvr, "namespace", namespace, "name", name)
+	gvrw, ok := f.watches[gvr]
+	if !ok {
+		panic(fmt.Errorf("Can't create event for missing watch on gvr: %v", gvr))
+	}
+	watch, ok := gvrw[namespace]
+	if !ok {
+		panic(fmt.Errorf("Can't create event for missing watch on gvr/namespace: %v/%v", gvr, namespace))
+	}
+	if err := watch.handler(name); err != nil {
+		panic(fmt.Errorf("Failed to run watch handler: %v", err))
+	}
+}
+
+// Returns true if there is no watch currently set. That's useful to
+// check at the end of a test if a Watch is still active.
+func (f *FakeWatchManager) Empty() bool {
+	return len(f.watches) == 0
+}
+
+type FakeWatch struct {
+	manager   *FakeWatchManager
+	gvr       schema.GroupVersionResource
+	namespace string
+	handler   func(name string) error
+	log       logr.Logger
+}
+
+func (f *FakeWatch) Stop() {
+	f.log.Info("Stop", "gvr", f.gvr, "namespace", f.namespace)
+	if gvrw, ok := f.manager.watches[f.gvr]; ok {
+		if _, ok := gvrw[f.namespace]; !ok {
+			panic(fmt.Errorf("Watch doesn't exist for %v/%v", f.gvr, f.namespace))
+		}
+		delete(gvrw, f.namespace)
+	} else {
+		panic(fmt.Errorf("Watch doesn't exist for %v", f.gvr))
+	}
+	if len(f.manager.watches[f.gvr]) == 0 {
+		delete(f.manager.watches, f.gvr)
+	}
+}
+
+type Counter map[string]int
+
+func (c Counter) Handler(name string) func() error {
+	return func() error {
+		c[name] += 1
+		return nil
+	}
+}
+
+func (c Counter) Check(t *testing.T, name string, wanted int) {
+	t.Helper()
+	if got := c[name]; got != wanted {
+		t.Fatalf("Expected count for %q = %v, got %v", name, wanted, got)
+	}
+}
+
+func TestNamedWatcher_CreateWatchDelete(t *testing.T) {
+	fakeManager := NewFakeWatchManager(testr.New(t))
+	watcher := controllers.NewNamedWatcher(fakeManager, testr.New(t))
+	counts := Counter{}
+
+	pod1, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1", counts.Handler("pod1"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod2")
+	counts.Check(t, "pod1", 1)
+	counts.Check(t, "pod2", 0)
+	pod1.Stop()
+	if !fakeManager.Empty() {
+		t.Fatalf("Some watches are left running: %v", fakeManager)
+	}
+}
+
+func TestNamedWatcher_MultipleWithSameNamespace(t *testing.T) {
+	fakeManager := NewFakeWatchManager(testr.New(t))
+	watcher := controllers.NewNamedWatcher(fakeManager, testr.New(t))
+	counts := Counter{}
+
+	pod1, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1", counts.Handler("pod1"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod2")
+	counts.Check(t, "pod1", 1)
+	counts.Check(t, "pod2", 0)
+	// Add new watch for the same combination
+	pod2, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod2", counts.Handler("pod2"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod2")
+	counts.Check(t, "pod1", 2)
+	counts.Check(t, "pod2", 1)
+
+	pod1.Stop()
+	pod2.Stop()
+
+	if !fakeManager.Empty() {
+		t.Fatalf("Some watches are left running: %v", fakeManager)
+	}
+}
+
+func TestNamedWatcher_MultipleWithSameEverything(t *testing.T) {
+	fakeManager := NewFakeWatchManager(testr.New(t))
+	watcher := controllers.NewNamedWatcher(fakeManager, testr.New(t))
+	counts := Counter{}
+
+	pod1, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1", counts.Handler("pod1"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	counts.Check(t, "pod1", 1)
+	counts.Check(t, "pod1bis", 0)
+	// Add new watch for the same combination
+	pod1bis, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1", counts.Handler("pod1bis"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	counts.Check(t, "pod1", 2)
+	counts.Check(t, "pod1bis", 1)
+
+	pod1.Stop()
+	pod1bis.Stop()
+
+	if !fakeManager.Empty() {
+		t.Fatalf("Some watches are left running: %v", fakeManager)
+	}
+}
+
+func TestNamedWatcher_DifferentNamespace(t *testing.T) {
+	fakeManager := NewFakeWatchManager(testr.New(t))
+	watcher := controllers.NewNamedWatcher(fakeManager, testr.New(t))
+	counts := Counter{}
+
+	pod1, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1", counts.Handler("pod1"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	counts.Check(t, "pod1", 1)
+	counts.Check(t, "namespace/pod2", 0)
+	// Add new watch for the same combination
+	pod2, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "namespace", "pod2", counts.Handler("namespace/pod2"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "namespace", "pod2")
+	counts.Check(t, "pod1", 2)
+	counts.Check(t, "namespace/pod2", 1)
+
+	pod1.Stop()
+	pod2.Stop()
+	if !fakeManager.Empty() {
+		t.Fatalf("Some watches are left running: %v", fakeManager)
+	}
+}
+
+func TestNamedWatcher_DifferentTypes(t *testing.T) {
+	fakeManager := NewFakeWatchManager(testr.New(t))
+	watcher := controllers.NewNamedWatcher(fakeManager, testr.New(t))
+	counts := Counter{}
+
+	pod1, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1", counts.Handler("pod1"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod2")
+	counts.Check(t, "pod1", 1)
+	counts.Check(t, "pod2", 0)
+
+	service1, err := watcher.Watch(schema.GroupVersionResource{Version: "v1", Resource: "services"}, "default", "service1", counts.Handler("service1"))
+	if err != nil {
+		t.Fatalf("Failed to start watch: %v", err)
+	}
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "pods"}, "default", "pod1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "services"}, "default", "service1")
+	fakeManager.Event(schema.GroupVersionResource{Version: "v1", Resource: "services"}, "default", "service2")
+	counts.Check(t, "pod1", 2)
+	counts.Check(t, "pod2", 0)
+	counts.Check(t, "service1", 1)
+	counts.Check(t, "service2", 0)
+
+	pod1.Stop()
+	service1.Stop()
+	if !fakeManager.Empty() {
+		t.Fatalf("Some watches are left running: %v", fakeManager)
+	}
+}
