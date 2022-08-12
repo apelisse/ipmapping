05e6172c3464e1c1c89832c8ce12ec1bf341082f
Antoine Pelisse <apelisse@gmail.com>
2022-08-12 10:34:08 -0700

    Split controller into two
    There's now one controller that applies the service and endpoints,
    and a controller that updates the status of the IPMapping object
    based on changes to the target object.

--- controllers/ipmapping_controller.go
+++ controllers/ipmapping_controller.go
@@ -22,14 +22,9 @@ import (
 
 	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/tools/cache"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -107,79 +102,18 @@ func (r *IPMappingReconciler) ApplyEndpoints(ctx context.Context, ipMapping *cha
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      ipMapping.ObjectMeta.Name,
 			Namespace: ipMapping.ObjectMeta.Namespace,
-		},
-		Subsets: []v1.EndpointSubset{
-			{
-				Addresses: []v1.EndpointAddress{
-					{
-						IP: *ipMapping.Status.IPAddress,
-					},
-				},
-				Ports: []v1.EndpointPort{
-					{
-						Port: 443, // TODO: Shouldn't be hard-coded
-					},
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion: ipMapping.APIVersion,
+					Kind:       ipMapping.Kind,
+					Name:       ipMapping.Name,
+					UID:        ipMapping.UID,
 				},
 			},
 		},
+		Subsets: subsets,
 	}
 	return r.Patch(ctx, &service, client.Apply, client.FieldOwner("ipmapping_controller"), client.ForceOwnership)
-}
-
-// Apply the given ipAddress to the status of the IPMapping. If the
-// ipAddress pointer is nil, this will force to remove the ipMapping
-// from the status, forcing the removal of the endpoint object.
-func (r *IPMappingReconciler) ApplyIPMappingStatus(ctx context.Context, nn types.NamespacedName, ipAddress *string) error {
-	mapping := changegroupv1beta1.IPMapping{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "IPMapping",
-			APIVersion: changegroupv1beta1.GroupVersion.String(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      nn.Name,
-			Namespace: nn.Namespace,
-		},
-		Status: changegroupv1beta1.IPMappingStatus{
-			IPAddress: ipAddress,
-		},
-	}
-	return r.Status().Patch(ctx, &mapping, client.Apply, client.FieldOwner("ipmapping_controller"), client.ForceOwnership)
-}
-
-func (r IPMappingReconciler) DeleteService(ctx context.Context, ipMapping *changegroupv1beta1.IPMapping) error {
-	service := v1.Service{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Service",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      ipMapping.ObjectMeta.Name,
-			Namespace: ipMapping.ObjectMeta.Namespace,
-		},
-	}
-	return r.Delete(ctx, &service)
-}
-
-func (r IPMappingReconciler) DeleteEndpoints(ctx context.Context, ipMapping *changegroupv1beta1.IPMapping) error {
-	endpoint := v1.Endpoints{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Endpoints",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      ipMapping.ObjectMeta.Name,
-			Namespace: ipMapping.ObjectMeta.Namespace,
-		},
-	}
-	return r.Delete(ctx, &endpoint)
-}
-
-func findField(obj runtime.Object, path *string) (*string, error) {
-	if path == nil {
-		return nil, nil
-	}
-	p := ParsePath(*path)
-	return p.Find(obj.(*unstructured.Unstructured))
 }
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
@@ -189,24 +123,9 @@ func (r *IPMappingReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	log.Info("Reconciling")
 
-	// TODO: I'm not sure this should be here (or way further, below).
-	// Update the watch and configure it to update the mapping status.
-	if watch, ok := r.watches[req.NamespacedName]; ok {
-		log.Info("Deleting watch")
-		watch.Stop()
-		delete(r.watches, req.NamespacedName)
-	}
-
 	var ipMapping changegroupv1beta1.IPMapping
+
 	if err := r.Get(ctx, req.NamespacedName, &ipMapping); err != nil {
-		if apierrors.IsNotFound(err) {
-			if r.DeleteService(ctx, &ipMapping); err != nil {
-				return ctrl.Result{}, fmt.Errorf("failed to delete service: %v", err)
-			}
-			if r.DeleteEndpoints(ctx, &ipMapping); err != nil {
-				return ctrl.Result{}, fmt.Errorf("failed to delete service: %v", err)
-			}
-		}
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
@@ -216,30 +135,6 @@ func (r *IPMappingReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	if err := r.ApplyEndpoints(ctx, &ipMapping); err != nil {
 		return ctrl.Result{}, fmt.Errorf("Failed to apply endpoints: %v", err)
 	}
-
-	// TODO: This shouldn't use the unsafe form.
-	gvr, _ := meta.UnsafeGuessKindToResource(schema.FromAPIVersionAndKind(ipMapping.Spec.TargetRef.APIVersion, ipMapping.Spec.TargetRef.Kind))
-	watch, err := r.watcher.Watch(gvr, req.Namespace, ipMapping.Spec.TargetRef.Name, func(lister cache.GenericLister) error {
-		log := log.WithName("WatchHandler")
-		log.Info("Event received", "gvr", gvr, "namespace", req.Namespace, "name", req.Name)
-		obj, err := lister.Get(req.Name)
-		var ipAddress *string
-		if err != nil && !apierrors.IsNotFound(err) {
-			return err
-		} else if err != nil {
-			ipAddress, err = findField(obj, ipMapping.Spec.TargetRef.FieldPath)
-			if err != nil {
-				return err
-			}
-		} else {
-			log.Info("Target deleted", "gvr", gvr, "namespace", req.Namespace, "name", req.Name)
-		}
-		return r.ApplyIPMappingStatus(ctx, req.NamespacedName, ipAddress)
-	})
-	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("Failed to create watch: %v", err)
-	}
-	r.watches[req.NamespacedName] = watch
 
 	return ctrl.Result{}, nil
 }
--- main.go
+++ main.go
@@ -85,6 +85,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "IPMapping")
 		os.Exit(1)
 	}
+	if err = (&controllers.IPMappingStatusReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr, ctrl.Log); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "IPMapping")
+		os.Exit(1)
+	}
+
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
